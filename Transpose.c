4fc456de06aa5d800683ab286a326ece444f6010
TanishaSaini28 <sainitanisha091@gmail.com>
2026-07-31 12:12:36 +0530

    Update Transpose.c

--- Transpose.c
+++ Transpose.c
@@ -1,17 +1,42 @@
-#include<stdio.h>
-int main(){
-    int r , c ; 
-    scanf("%d %d", &r , &c);
-    int arr[r][c];
-    for (int i = 0 ; i< r ; i++){
-        for (int j=0 ; j < c ; j++){
-            scanf("%d", &arr[i][j]);
+#include <stdio.h>
+void transposeMatrix(int r, int c, int matrix[][c], int transposed[][r])
+{
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            transposed[j][i] = matrix[i][j];
         }
     }
-    for (int i = 0 ; i< c ; i++){
-        for (int j=0 ; j < r ; j++){
-            printf("%d ", arr[j][i]);
+}
+
+void printMatrix(int r, int c, int matrix[][c])
+{
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
+}
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+    int matrix[r][c];
+    int transposed[c][r];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("original matrix \n");
+    printMatrix(r, c, matrix);
+    transposeMatrix(r, c, matrix, transposed);
+    printf("transposed matrix \n");
+    printMatrix(c, r, transposed);
 }
